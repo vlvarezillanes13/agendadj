48905d631fa3797acceae32a13fdf5a5555803fd
Vicente <v.lvarez.illanes@gmail.com>
2021-07-14 14:51:18 -0400

    Inicio listar usuarios

--- agenda/applications/persona/views.py
+++ agenda/applications/persona/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 
+from django.views.generic import ListView
+
+from .models import Person
+
+class ListaPersona(ListView):
+    model = Person
+    template_name = "persona/personas.html"
+    context_object_name = 'personas'
+
+    def get_queryset(self):
+        return Person.objects.all()
 
 
 
